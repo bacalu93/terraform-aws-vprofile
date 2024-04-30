02576bc4363d02eb031a5eec883246d2adf1a2bd
bacalu93 <andrei.bacalu93@gmail.com>
2024-04-30 11:41:58 +0200

    add vpc

---
+++ vpc.tf
@@ -0,0 +1,29 @@
+module "vpc" {
+    source = "terraform-aws-modules/vpc/aws"
+
+    name = var.VPC_NAME
+    cidr = var.VpcCIDR
+    azs = [var.ZONE1, var.ZONE2, var.ZONE3]
+    private_subnets = [var.PprivSUb1CIDR, var.PrivSUb2CIDR, var. PrivSUb3CIDR]
+    public_subnets = [var.PubSUb1CIDR, var.PubSUb2CIDR, var.PubSUb3CIDR]
+
+    enable_dns_hostnames = true
+    enable_dns_support   = true
+
+    enable_nat_gateway = true
+    single_nat_gateway = true
+
+
+    tags = {
+        Terraform = "true"
+        Environment = "Prod"
+
+    }
+    vpc_tags = {
+        Name = var.VPC_NAME
+    }
+
+
+
+
+}
