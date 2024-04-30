006982f46e3284bf8c2651c87578eb472ace5951
bacalu93 <andrei.bacalu93@gmail.com>
2024-04-30 13:42:18 +0200

    add SG

---
+++ secgrp.tf
@@ -0,0 +1,100 @@
+resource "aws_security_group" "vprofile-bean-elb-sg" {
+  name        = "vprofile-bean-elb-sg"
+  description = "Security group for bean-elb"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+}
+
+resource "aws_security_group" "vprofile-bastion-sg" {
+  name        = "vprofile-bastion-sg"
+  description = "Security group for bean-elb"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+}
+
+resource "aws_security_group" "vprofile-prod-sg" {
+  name        = "vprofile-prod-sg"
+  description = "Security group for bean-ec2"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.vprofile-bastion-sg.id]
+  }
+
+
+}
+
+resource "aws_security_group" "vprofile-backend-sg" {
+  name        = "vprofile-backend-sg"
+  description = "Security group for RDS, active MQ, elastic cache"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.vprofile-prod-sg.id]
+  }
+
+
+}
+
+resource "aws_security_group_rule" "sec_group_allow_itself" {
+  type                      = "ingress"
+  from_port                 = 0
+  to_port                   = 65535
+  protocol                  = "tcp"
+  security_group_id         = aws_security_group.vprofile-backend-sg.id
+  source_security_group_id  = aws_security_group.vprofile-backend-sg.id
+}
+
+
--- vpc.tf
+++ vpc.tf
@@ -1,27 +1,27 @@
 module "vpc" {
-    source = "terraform-aws-modules/vpc/aws"
+  source = "terraform-aws-modules/vpc/aws"
 
-    name = var.VPC_NAME
-    cidr = var.VpcCIDR
-    azs = [var.ZONE1, var.ZONE2, var.ZONE3]
-    private_subnets = [var.PprivSUb1CIDR, var.PrivSUb2CIDR, var. PrivSUb3CIDR]
-    public_subnets = [var.PubSUb1CIDR, var.PubSUb2CIDR, var.PubSUb3CIDR]
+  name            = var.VPC_NAME
+  cidr            = var.VpcCIDR
+  azs             = [var.ZONE1, var.ZONE2, var.ZONE3]
+  private_subnets = [var.PprivSUb1CIDR, var.PrivSUb2CIDR, var.PrivSUb3CIDR]
+  public_subnets  = [var.PubSUb1CIDR, var.PubSUb2CIDR, var.PubSUb3CIDR]
 
-    enable_dns_hostnames = true
-    enable_dns_support   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-    enable_nat_gateway = true
-    single_nat_gateway = true
+  enable_nat_gateway = true
+  single_nat_gateway = true
 
 
-    tags = {
-        Terraform = "true"
-        Environment = "Prod"
+  tags = {
+    Terraform   = "true"
+    Environment = "Prod"
 
-    }
-    vpc_tags = {
-        Name = var.VPC_NAME
-    }
+  }
+  vpc_tags = {
+    Name = var.VPC_NAME
+  }
 
 
 
